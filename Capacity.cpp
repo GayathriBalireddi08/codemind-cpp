ed60e495bbae042cf767742072060b49495f965c
GayathriBalireddi08 <119841081+GayathriBalireddi08@users.noreply.github.com>
2023-08-28 21:42:33 +0530

    #include<iostream> using namespace std; int main() {     int T,S,B;     cin>>T>>S>>B;     int c,cb;     c=2*T*S*B*512;     cb=c/1024 ;     cout<<cb<<" "<<"KB"<<endl;
    }

---
+++ Capacity.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,S,B;
+    cin>>T>>S>>B;
+    int c,cb;
+    c=2*T*S*B*512;
+    cb=c/1024 ;
+    cout<<cb<<" "<<"KB"<<endl;
+    
+    
+}
